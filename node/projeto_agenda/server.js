16c100f47d8719b794f4c01b3d1cf32c1a4e67d6
Genival Dantas <genineto10@gmail.com>
2021-11-20 17:19:11 -0300

    Agora funcionou

--- node/projeto_agenda/server.js
+++ node/projeto_agenda/server.js
@@ -47,7 +47,7 @@ app.use(routes);
 
 app.on('pronto', () => {
   app.listen(3000, () => {
-    console.log('Acessar http://localhost:3000/login/index');
+    console.log('Acessar http://localhost:3000');
     console.log('Servidor executando na porta 3000 ');
   });
 });
